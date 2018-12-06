cd91b5c71e12915dcfee129ad5d8f20726d3bf4c
Your Name <you@example.com>
2018-12-06 15:14:57 +0100

    merge botinit into main.go

--- main.go
+++ main.go
@@ -29,11 +29,38 @@ func main() {
 
 	fmt.Printf("Started bot %s: (%s) \n", user.Username, user.ID)
 
-	BotInit(discord, botConfig)
+	botInit(discord, botConfig)
 
 	// prevent process from exiting
 	<-make(chan struct{})
 }
+
+func botInit(discord *discordgo.Session, config *BotConfig) {
+	// prepare
+	threadHandler := CreateThreadHandler()
+	threadHandler.StartWatcher()
+
+	registry := &BotRegistry{ThreadHandler: threadHandler, BotConfig: config}
+	messageHandler := CreateMessageHandler()
+	messageHandler.BotRegistry = registry
+
+	messageHandler.RegisterReceiver(new(PostCommand))
+	messageHandler.RegisterReceiver(new(JokeCommand))
+	messageHandler.RegisterReceiver(new(CatCommand))
+	messageHandler.RegisterReceiver(new(AnswerCommand))
+	messageHandler.RegisterReceiver(new(ThreadCommand))
+	messageHandler.RegisterReceiver(new(SelfbanCommand))
+
+	discord.AddHandler(messageHandler.OnMessage)
+	discord.AddHandler(func(session *discordgo.Session, message *discordgo.MessageCreate) {
+		guild, _ := session.State.Guild(message.GuildID)
+		channel, _ := session.State.Channel(message.ChannelID)
+		go fmt.Println(fmt.Sprintf("[%s]-#%s (%s): %s", guild.Name, channel.Name, message.Author.Username, message.Content))
+	})
+
+	discord.Open()
+}
+
 
 func parseBotConfig() *BotConfig {
 	botConfig := BotConfig{}
