3b7e98477ea1c265a5f4bfd901a0372076973a19
Your Name <you@example.com>
2018-12-06 13:48:56 +0100

    download images before embedding

--- botcommand_post.go
+++ botcommand_post.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"github.com/bwmarrin/discordgo"
+	"io/ioutil"
 	"jaytaylor.com/html2text"
+	"net/http"
+	"os"
 	"strconv"
 	"strings"
 )
@@ -19,6 +24,28 @@ func (h *PostCommand) Exec(context *MessageContext) {
 	postId, _ := strconv.Atoi(strings.Trim(strings.Replace(context.Message.Content, "!post ", "", -1), " "))
 	post := context.BotRegistry.ThreadHandler.GetPost(postId)
 
+	if post == nil {
+		context.Session.ChannelMessageSend(context.Message.ChannelID,
+			"this post is not in my registry")
+
+		return
+	}
+
+	fileName := ""
+	// download image
+	if post.FileUrl != "" {
+		fileName = strconv.Itoa(post.PostId)+".jpg"
+
+		resp, _ := http.Get(post.FileUrl)
+		defer resp.Body.Close()
+
+		fbytes, _ := ioutil.ReadAll(resp.Body)
+		fpointer, _ := os.Create(fileName)
+		writer := bufio.NewWriter(fpointer)
+		writer.Write(fbytes)
+		writer.Flush()
+	}
+
 	postText, _ := html2text.FromString(post.Content)
 
 	embed := &discordgo.MessageEmbed{
@@ -31,13 +58,27 @@ func (h *PostCommand) Exec(context *MessageContext) {
 				Inline: false,
 			},
 		},
-		Image: &discordgo.MessageEmbedImage{
-			URL: post.FileUrl,
-		},
 		Thumbnail: &discordgo.MessageEmbedThumbnail{},
 		Title:     strconv.Itoa(post.PostId),
 	}
 
+	message := &discordgo.MessageSend{
+		Embed:embed,
+	}
+
+	if fileName != "" {
+		f, _ := os.Open(fileName)
+		embed.Image = &discordgo.MessageEmbedImage{
+			URL: fmt.Sprintf("attachment://%s", fileName),
+		}
+		message.Files = []*discordgo.File{
+			&discordgo.File{
+				Name: fileName,
+				Reader: f,
+			},
+		}
+	}
+
 	// post into #csg-reviews
-	context.Session.ChannelMessageSendEmbed("491172628917256192", embed)
+	context.Session.ChannelMessageSendComplex("491172628917256192", message)
 }
