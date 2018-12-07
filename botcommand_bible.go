27442cbe8a5b09f2b4149bae1dfa54005fa7c7ca
bergerhausen <mauricebergerhausen@gmail.com>
2018-12-07 16:44:03 +0100

    Added functionality to !bible command If user input is correct, the bot will fetch response from dailyverses.net with book, chapter and verse from user input.
    !help command output has been updated with correct usage

--- botcommand_bible.go
+++ botcommand_bible.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"jaytaylor.com/html2text"
 	"net/http"
@@ -8,7 +9,6 @@ import (
 )
 
 type BibleCommand struct {
-
 }
 
 func (b *BibleCommand) Satisfies(context *MessageContext) bool {
@@ -16,18 +16,41 @@ func (b *BibleCommand) Satisfies(context *MessageContext) bool {
 }
 
 func (b *BibleCommand) Exec(context *MessageContext) {
-	resp, _ :=  http.Get("https://dailyverses.net/random-bible-verse")
+	msgLen := len(strings.Split(context.Message.Content, " "))
+	if msgLen > 1 {
+		b.getBibleVerse(context)
+	} else {
+		b.randomBibleVerse(context)
+	}
+}
+
+func (b *BibleCommand) getBibleVerse(context *MessageContext) {
+	msgBook := strings.Split(context.Message.Content, " ")[1]
+	msgChpt := strings.Split(strings.Split(context.Message.Content, " ")[2], ":")[0]
+	msgVrs := strings.Split(strings.Split(context.Message.Content, " ")[2], ":")[1]
+
+	url := fmt.Sprintf("https://dailyverses.net/%s/%s/%s", msgBook, msgChpt, msgVrs)
+	resp, _ := http.Get(url)
 	doc, _ := goquery.NewDocumentFromReader(resp.Body)
 	htmlVerse, _ := doc.Find("div.bibleVerse").First().Html()
-
-	txt , _ := html2text.FromString(htmlVerse)
-
+	txt, _ := html2text.FromString(htmlVerse)
 	verse := strings.Split(txt, "(")[0]
+	context.Session.ChannelMessageSend(context.Message.ChannelID, verse)
+}
 
+func (b *BibleCommand) randomBibleVerse(context *MessageContext) {
+	resp, _ := http.Get("https://dailyverses.net/random-bible-verse")
+	doc, _ := goquery.NewDocumentFromReader(resp.Body)
+	htmlVerse, _ := doc.Find("div.bibleVerse").First().Html()
+	txt, _ := html2text.FromString(htmlVerse)
+	verse := strings.Split(txt, "(")[0]
 	context.Session.ChannelMessageSend(context.Message.ChannelID, verse)
 }
 
 func (b *BibleCommand) Info() string {
 	return `**!bible** 
-returns a random bible verse`
+returns a random bible verse 
+**!bible _book_ _chapter:verse_**
+returns a specific bible verse
+refer to <https://dailyverses.net/bible-books> to see all available books and verses`
 }
